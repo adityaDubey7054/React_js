ced64d9ba12b017b6bf49415038d3ef7f83ca9da
adityaDubey7054 <dubeyaditya7054@gmail.com>
2026-03-15 07:15:04 +0530

    update

--- tail_Implement/src/App.jsx
+++ tail_Implement/src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+ function Scrolling(elem){
+  if(elem>1){
+    console.log("too fast",elem)
+  }
+ }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onWheel={(elem)=>{
+      Scrolling(elem.deltaY)
+    }}>
+      <div className='h-screen w-full bg-amber-600'>Page1</div>
+      <div className='h-screen w-full bg-green-600'>Page2</div>
+      <div className='h-screen w-full bg-blue-600'>Page3</div>
+    </div>
   )
 }
 
